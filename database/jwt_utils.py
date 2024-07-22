2ed608f54c76dcc463db73f537228c28dbd6fa12
ogone4ek2090 <ahmtimur30@gmail.com>
2024-07-23 02:40:44 +0500

    del class user

--- database/jwt_utils.py
+++ database/jwt_utils.py
@@ -1,12 +1,10 @@
 import jwt
-from database.work import get_discord_id
 
 from config import JWT_SECRET
 
 
 async def gen_jwt(id):
-    discord_id = await get_discord_id(id)
-    return jwt.encode({"id": id, "discord_id": discord_id}, JWT_SECRET, algorithm="HS256")
+    return jwt.encode({"id": id}, JWT_SECRET, algorithm="HS256")
 
 
 def decode_jwt(token):
