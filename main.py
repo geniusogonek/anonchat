78fbab2b814e2ae1fa13f4871632b34b6f129318
ogone4ek2090 <ahmtimur30@gmail.com>
2024-07-23 03:34:16 +0500

    new manager class

--- main.py
+++ main.py
@@ -11,26 +11,42 @@ app = FastAPI()
 templates = Jinja2Templates("templates")
 
 
-class ConnectionManager():
-    def __init__(self) -> None:
-        self.active_connections: List[WebSocket] = []
+class AnonChatManager:
+    def __init__(self):
+        self.active_connects: List[List[WebSocket]] = []
+        self.waiting: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
+        await websocket.receive()
 
+        self.waiting.append(websocket)
+        if len(self.waiting) == 2:
+            (self.send(conn, "companion is founded!") for conn in self.waiting)
+            self.active_connects.append(self.waiting.pop())
+    
     def disconnect(self, websocket):
-        self.active_connections.remove(websocket)
+        for index, (conn1, conn2) in enumerate(self.active_connects):
+            if websocket == conn1:
+                self.send(conn2, "companion_leave_you")
+            elif websocket == conn2:
+                self.send(conn1, "companion_leave_you")
+            else:
+                continue
+            del self.active_connects[index]
 
-    async def send(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    def disconnect_from_waiting(self, websocket):
+        if websocket in self.waiting:
+            self.waiting.remove(websocket)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+    async def send(websocket: WebSocket, text: str):
+        await websocket.send_text(text)
 
-
-manager = ConnectionManager()
+    def get_companion(self, websocket):
+        for conn1, conn2 in self.active_connects:
+            if websocket == conn1:
+                return conn2
+            if websocket == conn2:
+                return conn1
 
 
 @app.get("/chat")
@@ -41,18 +57,19 @@ async def chat(request: Request):
         context={"is_auth": await work.check_auth(request)}
     )
 
-
-@app.websocket("/ws")
-async def websocket_connect(websocket: WebSocket, request: Request):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send(f"СОБЕСЕДНИК: {data}", await work.get_companion(request))
-            await manager.send(f"ВЫ: {data}", websocket)
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.send("СОБЕСЕДНИК ПОКИНУЛ ЧАТ, ОБЩЕНИЕ ЗАВЕРШЕНО", await work.get_companion(request))
+# TODO
+# ПЕРЕПИСАТЬ
+#@app.websocket("/ws")
+#async def websocket_connect(websocket: WebSocket, request: Request):
+#    await manager.connect(websocket)
+#    try:
+#        while True:
+#            data = await websocket.receive_text()
+#            await manager.send(f"СОБЕСЕДНИК: {data}", await work.get_companion(request))
+#            await manager.send(f"ВЫ: {data}", websocket)
+#    except WebSocketDisconnect:
+#        manager.disconnect(websocket)
+#        await manager.send("СОБЕСЕДНИК ПОКИНУЛ ЧАТ, ОБЩЕНИЕ ЗАВЕРШЕНО", await work.get_companion(request))
 
 
 async def main():
